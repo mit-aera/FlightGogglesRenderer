30e810f0a5dba53446d59e2b28486468bab8b803
Winter Guerra <winterg@mit.edu>
2019-02-25 17:30:33 -0500

    Added script for disabling realtime probes if not supported.

---
+++ FlightGoggles/Scripts/realtimeProbeDisabler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Disables realtime reflection probes if not supported.
+
+public class realtimeProbeDisabler : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Check if realtime reflection probes are disabled.
+        if (!QualitySettings.realtimeReflectionProbes){
+            // Disable this realtime probe.
+            this.gameObject.enabled = false;
+        }
+    }
+
+}
