f9dde720e6c6844153753a5b958df616f8c57cac
Winter Guerra <winterg@mit.edu>
2019-02-27 14:05:58 -0500

    Added instance idx to CLI.

---
+++ FlightGoggles/Scripts/linuxObjectDisabler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Disables baked reflection probes for Linux (not well supported)
+
+public class linuxObjectDisabler : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Check runtime OS
+        if (Application.platform == RuntimePlatform.LinuxPlayer){
+            // Disable this object.
+            this.gameObject.enabled = false;
+        }
+    }
+
+}
